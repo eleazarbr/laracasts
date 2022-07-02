47f49c4cf6839e2bc825cae9a206e89e049d7b1a
Eleazar Resendez <e.resendez@outlook.com>
2022-07-02 09:59:23 -0500

    Lazy Collections

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        ini_set("memory_limit", "-1");
+        User::truncate();
+        User::factory()
+            ->count(500000)
+            ->create();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,24 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    // It will throw the "Allowed memory size of bytes exhausted" error.
+    // $users = User::get()->filter(function ($user) {
+    //     return $user->id < 250000;
+    // });
+
+    // Creates a lazy collection.
+    // The filter callback is not executed until we actually iterate over
+    // each user individually, allowing for a drastic reduction in memory usage.
+    $users = User::cursor()
+        ->filter(function ($user) {
+            return $user->id < 100;
+        })
+        ->take(100);
+
+    foreach ($users as $user) {
+        echo "$user->id \n";
+    }
 });
